fe8b4fac163b332fb8df0f5b2b5e2ca5493ead08
ericcarmi <ecarmi@protonmail.com>
2024-05-02 19:45:25 -0700

    this is a decent multi range slider

---
+++ src/zoomable-slider.svelte
@@ -0,0 +1,222 @@
+<script lang="ts">
+	import { onMount } from "svelte";
+	import { MAX_FREQ, MIN_FREQ, NYQUIST } from "./constants.svelte";
+	let control_max_freq = NYQUIST;
+	let control_min_freq = 20;
+
+	export let value: number = control_max_freq / 2;
+	export let index: number = -1;
+	export let update_server = () => {};
+
+	let height = "1em";
+
+	let position = 0;
+	let el: HTMLElement;
+	let indicator: HTMLElement;
+	let is_dragging = false;
+	let width = 180;
+	let indicator_width = 10;
+
+	let high_res = 0.1;
+	let low_res = 0.5;
+	let resolution = 1;
+
+	function update_value() {
+		let x =
+			((position + indicator_width) / width) *
+				(control_max_freq - control_min_freq) +
+			control_min_freq;
+		value = x;
+	}
+
+	function update_position() {
+		position =
+			((value - control_min_freq) / (control_max_freq - control_min_freq)) *
+			width;
+		position = Math.max(Math.min(position, width - indicator_width), 0);
+		indicator.style.left = position + "px";
+	}
+
+	function draggable() {
+		if (el === null) {
+			return;
+		}
+		indicator.addEventListener("mousedown", function (e: MouseEvent) {
+			var offsetX = e.clientX - position;
+			function mouseMoveHandler(e: MouseEvent) {
+				if (el === null || !is_dragging) {
+					return;
+				}
+
+				position = e.clientX - offsetX;
+				position = Math.max(Math.min(position, width - indicator_width), 0);
+				indicator.style.left = position + "px";
+				update_value();
+			}
+			function reset() {
+				is_dragging = false;
+				update_server();
+				window.removeEventListener("mousemove", mouseMoveHandler);
+				window.removeEventListener("mouseup", reset);
+			}
+			window.addEventListener("mousemove", mouseMoveHandler);
+			window.addEventListener("mouseup", reset);
+		});
+		el.addEventListener("mousedown", function (e: MouseEvent) {
+			var offsetX = e.clientX - position;
+
+			function mouseMoveHandler(e: MouseEvent) {
+				if (el === null || !is_dragging) {
+					return;
+				}
+				let wrap_position = el.offsetLeft;
+				// change this and other stuff to not use constants that aren't based on the specified dimensions of the component
+				if (Math.abs(e.clientX - wrap_position) > 5) {
+					position = e.clientX - offsetX;
+					position = Math.max(Math.min(position, width - indicator_width), 0);
+				} else {
+					position = e.clientX - offsetX;
+					position = Math.max(Math.min(position, width - indicator_width), 0);
+				}
+				indicator.style.left = position + "px";
+
+				update_value();
+			}
+			function reset() {
+				// have to call this here...maybe want to change how this is handled later
+				is_dragging = false;
+				update_server();
+
+				window.removeEventListener("mousemove", mouseMoveHandler);
+				window.removeEventListener("mouseup", reset);
+			}
+			window.addEventListener("mousemove", mouseMoveHandler);
+			window.addEventListener("mouseup", reset);
+		});
+	}
+
+	onMount(() => {
+		draggable();
+	});
+</script>
+
+<div style="display: flex; flex-direction: column; gap: 4px;">
+	<div
+		class="wrapper"
+		style="width:{width}px; height:{height}"
+		bind:this={el}
+		data-attribute={is_dragging}
+		role="button"
+		tabindex={-1}
+		on:mousedown={(e) => {
+			is_dragging = true;
+			if (e.shiftKey) {
+			}
+			let wrap_position = el.offsetLeft;
+			if (Math.abs(e.clientX - wrap_position) > 5) {
+				position = e.clientX - wrap_position - 5;
+				position = Math.max(Math.min(position, width - indicator_width), 0);
+			} else {
+				position = e.clientY - wrap_position - 5;
+				position = Math.max(Math.min(position, width - indicator_width), 0);
+			}
+			indicator.style.left = position + "px";
+			update_value();
+		}}
+		on:mouseup={() => {
+			is_dragging = false;
+		}}
+	>
+		<div
+			class="thumb"
+			style="width:{indicator_width}px; height:{height}"
+			bind:this={indicator}
+			data-attribute={is_dragging}
+			role="button"
+			tabindex={-1}
+			on:mousedown={(e) => {
+				if (e.shiftKey) {
+					resolution = high_res;
+				}
+				is_dragging = true;
+			}}
+			on:mouseup={() => {
+				is_dragging = false;
+				resolution = low_res;
+			}}
+		/>
+		<span class="value-text">{value.toFixed(1)}</span>
+	</div>
+	<div>
+		<div
+			class="scroll-number"
+			style="float: left;"
+			on:wheel={(e) => {
+				e.preventDefault();
+				let v = e.deltaY * 5 + control_min_freq;
+				control_min_freq = Math.max(0, Math.min(v, value));
+				update_position();
+			}}
+		>
+			{control_min_freq.toFixed(0)}
+		</div>
+		<div
+			class="scroll-number"
+			style="float: right;"
+			on:wheel={(e) => {
+				e.preventDefault();
+				let v = e.deltaY * 10 + control_max_freq;
+				control_max_freq = Math.max(value, Math.min(v, NYQUIST));
+				update_position();
+			}}
+		>
+			{control_max_freq.toFixed(0)}
+		</div>
+	</div>
+</div>
+
+<style>
+	.wrapper {
+		background: var(--gray100);
+		display: flex;
+		justify-content: center;
+		border: 1px solid var(--slider-border);
+		position: relative;
+		transition: border 0.33s;
+	}
+	.wrapper:hover {
+		border: 1px solid var(--slider-hover);
+	}
+	.wrapper[data-attribute="true"] {
+		border: 1px solid var(--slider-hover);
+	}
+	.thumb {
+		background: black;
+		position: absolute;
+	}
+	.thumb:active {
+		background: var(--slider-active);
+	}
+	.thumb[data-attribute="true"] {
+		background: var(--slider-active);
+	}
+	.value-text {
+		position: relative;
+		top: -20%;
+		font-size: 12px;
+		color: var(--gray200);
+		pointer-events: none;
+		font-weight: bold;
+	}
+	.scroll-number {
+		background: rgba(0, 0, 0, 0.5);
+		border: 1px solid transparent;
+		transition: border-color 0.33s;
+		cursor: ns-resize;
+		width: 3em;
+		font-size: 12px;
+	}
+	.scroll-number:hover {
+		border-color: var(--rotary-tick);
+	}
+</style>
